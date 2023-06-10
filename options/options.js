b3011f70dd0f665d96aae54b457ab2852af015f4
Crowdbotics <team@crowdbotics.com>
2023-06-10 17:55:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-resonance-41406",
+  url: "https://round-resonance-41406.botics.co",
+  api: "https://round-resonance-41406.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
